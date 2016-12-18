d00b0938d931ee39b614d53fd67363e008af5aef
wellingtonxxl <wellington.acr@gmail.com>
2016-12-18 10:28:26 -0400

    atualizando diretorio Node files

---
+++ jsFiles/app.js
@@ -0,0 +1,22 @@
+var cluster = require ('cluster');
+
+if(cluster.isMaster){
+   //cont processors
+   var contProc = require('os').cpus().length;
+
+   //creating a working for each processor
+   for (var i = 0; i < contProc; i++ ){
+       cluster.fork();
+   }
+} else {
+
+   var express = require('express');
+   var app = express();
+
+   app.get('/', function (req, res){
+     res.send('Hello World!-!' +cluster.worker.id);
+   });
+
+  app.listen(3000);
+  console.log('Example app listening on port 3000! %d', cluster.worker.id);
+}
