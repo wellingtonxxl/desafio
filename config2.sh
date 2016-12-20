ec6181e4920d6156d5f2e278614c70ee18eae834
wellingtonxxl <wellington.acr@gmail.com>
2016-12-19 20:07:40 -0400

    script de instalacao e inicializacao de servicos

---
+++ config2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Shell Script
+#iniciando servicos
+
+ sudo apt-get install -y epel-release
+ sudo apt-get install -y npm
+ sudo npm install -y express
+ sudo apt-get install -y nginx
+ sudo npm install pm2@latest -g
+
+#starting apache2 and nginx
+sudo mkdir /home/ubuntu/desafio/
+sudo pm2 startup
+
+sudo /etc/init.d/apache2 start
+#sudo /etc/init.d/nginx start
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+sudo env PATH=$PATH:/usr/local/bin pm2 startup ubuntu -u sammy
+
+sudo pm2 start /home/ubuntu/desafio/clusters.js
+
